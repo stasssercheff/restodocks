8814c3d9629f4217e3f6021cfc2291b8797ca709
stasssercheff <stassserchef@gmail.com>
2025-12-07 09:37:51 +0700

    Initial Commit

---
+++ Restodocks/RestodocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestodocksApp.swift
+//  Restodocks
+//
+//  Created by Stanislav Rebrikov on 12/7/25.
+//
+
+import SwiftUI
+
+@main
+struct RestodocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
