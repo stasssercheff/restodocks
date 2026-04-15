4fcf5c11cea41b19251068509adaf8a3edb8a79a
stasssercheff <stasssercheff@users.noreply.github.com>
2026-04-15 13:34:33 +0700

    Revert "Use publishable Supabase key as default web anon key"
    This reverts commit 1b2d60d5bcb6de82d6d8732109ffa8a0098bde6e.

--- restodocks_flutter/lib/core/supabase_env.dart
+++ restodocks_flutter/lib/core/supabase_env.dart
@@ -7,7 +7,7 @@ const String kSupabaseUrlFromEnvironment = String.fromEnvironment(
 const String kSupabaseAnonKeyFromEnvironment = String.fromEnvironment(
   'SUPABASE_ANON_KEY',
   defaultValue:
-      'sb_publishable_VLi05Njkuzk_SBkLB_8j0A_00jr73Im',
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9zZ2xmcHR3YnVxcW1xdW50dGhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUwNTk0MDQsImV4cCI6MjA4MDYzNTQwNH0.Jy7yi2TNdSrmoBdILXBGRYB_vxGtq8scCZ9eCA9vfTE',
 );
 
 /// Базовый URL веб-приложения для ссылок из писем (подтверждение копирования заведения и т.д.).
